7a66f70f5a4f5f3c858532d95060eab2170d2d0e
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-03-20 22:15:55 -0300

    feat: :sparkles: cria middleware para validar schemas genêricos

---
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { ObjectSchema } from "joi"; //tipagem do schema joi
+
+export function validateSchemaMiddleware(schema: ObjectSchema) {
+
+    return (req: Request, res: Response, next: NextFunction) => {
+        const validation = schema.validate(req.body)
+
+        if (validation.error) {
+            return res.status(422).send(validation.error.details)
+        }
+
+        next()
+    }
+
+
+}
