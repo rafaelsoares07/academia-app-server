a8fe4f9674b6356822650202fadd62c89eed5f4a
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-03-21 16:02:17 -0300

    feat: :sparkles: adiciona rotas ligadas a entidade cliente no arquivo index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, { json } from "express";
 import academyAuthRouter from "./routes/auth/academyAuthRoutes";
 import clientAuthRouter from './routes/auth/clientAuthRouter';
 import instructorRouter from './routes/auth/instructorAuthRouter';
+import costumerRouter from './routes/costumer/costumerRouter';
 
 const app = express()
 app.use(json())
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(academyAuthRouter)
 app.use(clientAuthRouter)
 app.use(instructorRouter)
+app.use(costumerRouter)
 
 app.listen(5000, () => {
     console.log("Sevidor rodando:")
